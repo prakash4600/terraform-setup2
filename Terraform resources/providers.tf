4281b2294750f28613e3ff423b5a202ab8760ddb
Ashish Draksharapu <ashish.draksharapu@pacteraedge.com>
2023-08-08 10:43:08 +0000

    Updated providers.tf

--- Terraform resources/providers.tf
+++ Terraform resources/providers.tf
@@ -12,8 +12,5 @@ provider "azurerm" {
   features {
   }
 
-  client_id = "ebac3988-a142-4e6e-9c8a-2321ef080922"
-  subscription_id = "e38e4eed-8fbd-4713-b27d-0f419989008a"
-  client_secret = "Sx.8Q~kdCrD536IeTaJcLXSOXAe3CVy.6n7ngaFD"
-  tenant_id = "9b415834-803a-4da0-afdc-fe6b1d52d649"
+
 }
